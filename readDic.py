d035626b8fc5596f6e6ed8db97744280e8e70874
William Xu <xuweilu@shinetechchina.com>
2017-09-15 14:19:12 +0800

    remove negative characters.

--- readDic.py
+++ readDic.py
@@ -6,7 +6,7 @@ def main(searchText):
     wordExp = r"(?<=^\…)\w+(?=\＠)"
     with open('modern-chinese-dic.txt', 'r', encoding='utf-8') as f:
         modernDic = f.read()
-        testExpression = r'.*' + searchText + r'.*\n'
+        testExpression = r'.*(?<=[^(不)|(没有)])' + searchText + r'.*\n'
         results = re.findall(testExpression, modernDic)
         relatedWordsDic = {}
         relatedCharactersDic = {}
